95afb9d08679e96a3387965e7600c0adcda52f32
Kszemi <mkrzemien11@gmail.com>
2018-06-03 11:35:15 +0200

    test

--- ImageAnalysis/src/ImageAnalysis.java
+++ ImageAnalysis/src/ImageAnalysis.java
@@ -5,7 +5,6 @@ public class ImageAnalysis {
 	public static void main(String[] args) {
 		
 		File img1 = new File("/ImageAnalysis/photos/rsz_dsc_0279.png");
-		
 		File img2 = new File("/ImageAnalysis/photos/rsz_dsc_0280.png");
 		
 		
--- ImageAnalysis/src/Point.java
+++ ImageAnalysis/src/Point.java
@@ -3,5 +3,5 @@ public class Point
 {
 	float x;
 	float y;
-	
+	// a imie jego cztery 2 zero
 }
