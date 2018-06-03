33f3be2c39b646ef203d855d0c7a56173ceefada
sat227 <sat227@gmail.com>
2018-06-03 11:57:18 +0200

    point extension

--- ImageAnalysis/src/Point.java
+++ ImageAnalysis/src/Point.java
@@ -1,11 +1,43 @@
+import java.util.ArrayList;
 
 public class Point 
 {
+	static int count;
+	int id;
 	float x;
 	float y;
-	// a imie jego cztery 2 zero
 	short[] features = new short[128];
 	
+	
+	Point()
+	{
+		id=count+1;
+		count++;
+	}
+	
+	private Point getNearestPoint(ArrayList<Point> points)
+	{
+		int nearestDistance = 1000000000;
+		int nearestPointId = -1;
+		
+		for(int i=0; i<points.size(); i++)
+		{
+			if (nearestDistance > this.getDistance(points.get(i)))
+			{
+				
+				nearestPointId = points.get(i).id;
+				nearestDistance = this.getDistance(points.get(i));
+			}
+		}
+		
+		return points.get(nearestPointId);
+	}
+	
+	private int getDistance(Point point)
+	{
+		
+		return 0;
+	}
 	
 	
 }
