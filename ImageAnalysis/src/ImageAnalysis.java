da4aaaa1ee5e7379c57dcdb931fb8a3ac11d28ea
Kszemi <mkrzemien11@gmail.com>
2018-06-12 13:10:53 +0200

    z rysowaniem

--- ImageAnalysis/src/ImageAnalysis.java
+++ ImageAnalysis/src/ImageAnalysis.java
@@ -1,15 +1,27 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Set;
+
+import javax.imageio.ImageIO;
 
 public class ImageAnalysis {
 
-public static void main(String[] args) throws FileNotFoundException {
+public static void main(String[] args) throws IOException {
 		
-		File img1 = new File("photos/rsz_dsc_0283.png.haraff.sift");
-		File img2 = new File("photos/rsz_dsc_0284.png.haraff.sift");
+		File img1 = new File("photos/rsz_dsc_0285.png.haraff.sift");
+		File img2 = new File("photos/rsz_dsc_0286.png.haraff.sift");
 
+//		File img1 = new File("photos/a.png.haraff.sift");
+//		File img2 = new File("photos/b.png.haraff.sift");
+		
+		
 		Point[] points1 = PointCreator.createPoints(img1);
 		Point.resetCounter();
 		Point[] points2 = PointCreator.createPoints(img2);
@@ -27,7 +39,7 @@ public static void main(String[] args) throws FileNotFoundException {
 		ArrayList<Point> nearestPoints1 = new ArrayList<>();
 		ArrayList<Point> nearestPoints2 = new ArrayList<>();
 		
-		int neigbours = 10;
+		int neigbours =10 ;
 		
 		for(int i = 0; i < points1.length; i++){
 			 points1[i].calculateNearestPoints(new ArrayList<>(Arrays.asList(points1)), neigbours);
@@ -49,7 +61,7 @@ public static void main(String[] args) throws FileNotFoundException {
 		
 		int counter = 0;
 		int similarPointsCounter = 0;
-		double similarityThreshold = 0.7;
+		double similarityThreshold = 0.8;
 		for(int i = 0; i < pairPoints.size(); i++){
 			for(int j = 0; j < neigbours; j++){
 				if(pairPoints.get(i).getNearestPoints().contains(pairPoints.get(i).getNearestPoint().getNearestPoints().get(j).getNearestPoint())){
@@ -63,24 +75,42 @@ public static void main(String[] args) throws FileNotFoundException {
 			
 		}
 		
-		System.out.println("counter = " + counter);
-		System.out.println("Ratio: " + (double)counter/pairPoints.size());
+		System.out.println("Spójne s¹siedztwo, pary: " + counter);
+	
+		Ransac ransac = new Ransac(pairPoints);
+		ransac.run(300, 10, 300);
+		
+		paintPairs("rsz_dsc_0285.png", "rsz_dsc_0286.png", "85-86 RANSAC.png", ransac.getBestPoints());
+		
+		
 	
 		
-		
-		
-		Ransac ransac = new Ransac(pairPoints);
-		ransac.run(10);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
+private static void paintPairs(String image1Name, String image2Name, String outImageName, ArrayList<Point> pairPoints) throws IOException {
+    BufferedImage image1 = ImageIO.read(new File("photos/"+image1Name));
+    BufferedImage image2 = ImageIO.read(new File("photos/"+image2Name));
+    
+    if (image1.getHeight() != image2.getHeight() || image1.getWidth() != image2.getWidth()) {
+        System.out.println("Failed to save images. Different sizes");
+        return;
+    }
+
+    BufferedImage outImage = new BufferedImage(image1.getWidth(), image1.getHeight() * 2, BufferedImage.TYPE_INT_ARGB);
+
+    Graphics graphics = outImage.getGraphics();
+    graphics.drawImage(image1, 0, 0, null);
+    graphics.drawImage(image2, 0, image1.getHeight(), null);
+
+    Random rand = new Random();
+
+    for (Point p: pairPoints) {
+        graphics.setColor(new Color((int)(Math.random() * 0x1000000)));
+        graphics.drawLine((int)p.getX(), (int)p.getY(), (int)p.getNearestPoint().getX(), image1.getHeight() + (int)p.getNearestPoint().getY());
+    }
+
+    ImageIO.write(outImage, "PNG", new File("photos/"+outImageName));
+
+}
+
+
 }
--- ImageAnalysis/src/Ransac.java
+++ ImageAnalysis/src/Ransac.java
@@ -8,11 +8,21 @@ public class Ransac {
 
 	private ArrayList<Point> pairPoints;
 	
+	private ArrayList<Point> ransacPoints;
+	
+	private ArrayList<Point> bestPoints;
+	
+	public ArrayList<Point> getBestPoints() {
+		return bestPoints;
+	}
+
 	public Ransac(ArrayList<Point> pairPoints) {
 		this.pairPoints = pairPoints;
 	}
 	
-	public void run(int iterations){
+	public void run(int iterations, double min, double max){
+		
+		int threshold = 8;
 		
 		double a, b, c, d, e, f;
 		
@@ -23,16 +33,34 @@ public class Ransac {
 		Matrix matrixB;
 		Matrix modelParameterMatrix = null;
 		double error = 0; 
-		double minError = Integer.MAX_VALUE;
-		double maxError = 0;
+		int minCounter = Integer.MAX_VALUE;
+		int maxCounter = 0;
+		int counter = 0;
+		double distance = 0;
 		
-		for(int i = 0; i < iterations; i++){
+		for(int i = 0; i < 30; i++){
 			
 			for(int p1 = 0; p1 < pairPoints.size(); p1++){
 				
 				for(int p2 = p1 + 1; p2 < pairPoints.size(); p2++){
 					
+					distance = Math.hypot(pairPoints.get(p1).getX()-pairPoints.get(p2).getX(), pairPoints.get(p1).getY()-pairPoints.get(p2).getY());
+					if(distance < min || distance > max ){
+						continue;
+					}
+					
 					for(int p3 = p2 + 1; p3 < pairPoints.size(); p3++){
+						
+						distance = Math.hypot(pairPoints.get(p1).getX()-pairPoints.get(p3).getX(), pairPoints.get(p1).getY()-pairPoints.get(p3).getY());
+						if(distance < min || distance > max ){
+							continue;
+						}
+						
+						distance = Math.hypot(pairPoints.get(p2).getX()-pairPoints.get(p3).getX(), pairPoints.get(p2).getY()-pairPoints.get(p3).getY());
+						if(distance < min || distance > max ){
+							continue;
+						}
+						
 						
 								matrixTemp1[0][0] = pairPoints.get(p1).getX();
 								matrixTemp1[0][1] = pairPoints.get(p1).getY();
@@ -57,7 +85,6 @@ public class Ransac {
 								matrixTemp1[5][3] = pairPoints.get(p3).getX();
 								matrixTemp1[5][4] = pairPoints.get(p3).getY();
 								matrixTemp1[5][5] = 1;
-								
 								
 								matrixTemp2[0] = pairPoints.get(p1).nearestPoint.getX();
 								matrixTemp2[1] = pairPoints.get(p2).nearestPoint.getX();
@@ -84,33 +111,39 @@ public class Ransac {
 							        
 //							        double distance = Math.hypot(x1-x2, y1-y2);
 							        
-							        error = Math.hypot( (a * pairPoints.get(p1).getX() + b * pairPoints.get(p1).getY() + c) 
-							        		- pairPoints.get(p1).getNearestPoint().getX(),
-							        		(d * pairPoints.get(p1).getX() + e * pairPoints.get(p1).getY() + f)
-							        		- pairPoints.get(p1).getNearestPoint().getY());
-							        
-							        error += Math.hypot( (a * pairPoints.get(p2).getX() + b * pairPoints.get(p2).getY() + c) 
-							        		- pairPoints.get(p2).getNearestPoint().getX(),
-							        		(d * pairPoints.get(p2).getX() + e * pairPoints.get(p2).getY() + f)
-							        		- pairPoints.get(p2).getNearestPoint().getY());
-							        
-							        error += Math.hypot( (a * pairPoints.get(p3).getX() + b * pairPoints.get(p3).getY() + c) 
-							        		- pairPoints.get(p3).getNearestPoint().getX(),
-							        		(d * pairPoints.get(p3).getX() + e * pairPoints.get(p3).getY() + f)
-							        		- pairPoints.get(p3).getNearestPoint().getY());
-							        
-							        if(minError > error){
-							        	minError = error;
+							        counter = 0;
+							        ransacPoints = new ArrayList<>();
+							        for(int point = 0; point < pairPoints.size(); point++){
+							        	
+							        	error = estimateError(pairPoints.get(point), a, b, c, d, e, f);
+							        	
+							        	if(error <= threshold){
+							        		counter++;
+							        		ransacPoints.add(pairPoints.get(point));
+							        	}
 							        }
-							        if(maxError < error){
-							        	maxError = error;
+							        
+							        if(counter > maxCounter){
+							        	maxCounter = counter;
+							        	bestPoints = new ArrayList<>(ransacPoints);
+							        	
 							        }
+							        
 								}
 					}
 				}
 			}
 		}
-	System.out.println("maxError: " + maxError + " minError: " + minError);		
+	System.out.println("Affiniczna, s¹siedzi: " + maxCounter);		
 	}
 
+	public double estimateError(Point point, double a, double b, double c, double d, double e, double f) 
+	{
+		double error = Math.hypot( (a * point.getX() + b * point.getY() + c) 
+	        		- point.getNearestPoint().getX(),
+	        		(d * point.getX() + e * point.getY() + f)
+	        		- point.getNearestPoint().getY());
+		
+		return error;
+	}
 }
